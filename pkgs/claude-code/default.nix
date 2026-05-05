a13e8c4e3d3573c978a19e2e99b95e69aea049af
forgejo-actions <forgejo-actions@alc.xyz>
2026-05-05 06:03:26 +0000

    chore(assistant-cli): update to 2.1.128

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -9,14 +9,14 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "claude-code";
-  version = "2.1.92";
+  version = "2.1.128";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${finalAttrs.version}.tgz";
-    hash = "sha256-CLLCtVK3TeXFZ8wBnRRHNc2MoUt7lTdMJwz8sZHpkFM=";
+    hash = "sha256-cR6qGXBxU7rgzJUcxsmCUKqEcRQBWmFJgPxtSsXSW64=";
   };
 
-  npmDepsHash = "sha256-5LvH7fG5pti2SiXHQqgRxfFpxaXxzrmGxIoPR4dGE+8=";
+  npmDepsHash = "sha256-pncZCDdsezpNtExE6wVXvdTYAWbD3TZmvqSpCFo/KUA=";
 
   strictDeps = true;
 
