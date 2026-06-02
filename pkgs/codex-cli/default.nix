d08529462775570804cbb0524c53e4b4415cc710
alcxyz <alcxyz@noreply.git.alc.xyz>
2026-06-02 08:04:53 +0200

    chore(codex-cli): update to 0.136.0 (#81)

--- pkgs/codex-cli/default.nix
+++ pkgs/codex-cli/default.nix
@@ -5,14 +5,14 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "codex-cli";
-  version = "0.135.0";
+  version = "0.136.0";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@openai/codex/-/codex-${finalAttrs.version}.tgz";
-    hash = "sha256-7Kixtc3Es06gfKlFlRfaETaAGRVQnyOw272057agOiA=";
+    hash = "sha256-yP84Fy5bNUW6QGpOJJ+iQyBai+HtuddaUbPS8nrsDwE=";
   };
 
-  npmDepsHash = "sha256-DX5Nv1uuwXv3u0EHV6K7MxjIfCZyVcH48LfYJDRkv0E=";
+  npmDepsHash = "sha256-UHsymrmy/jmi1n9dSr1YHpAaxhIOCpzY1hau57rDa/A=";
 
   strictDeps = true;
 
