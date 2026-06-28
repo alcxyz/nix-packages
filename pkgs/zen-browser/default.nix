bcde45af4c7c09e81d5cda2b492a4bdfb8b68135
alcxyz <alcxyz@noreply.git.alc.xyz>
2026-06-28 08:04:24 +0200

    chore(zen-browser): update to 1.21.4b (#143)

--- pkgs/zen-browser/default.nix
+++ pkgs/zen-browser/default.nix
@@ -19,7 +19,7 @@
 }:
 
 let
-  version = "1.21.3b";
+  version = "1.21.4b";
 
   policies = {
     DisableAppUpdate = true;
@@ -35,8 +35,8 @@ let
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
 
       # Upstream (hex) SHA256:
-      # a0ceeaf931c15d7c37de4faef4ea247eeb10e4b85a9e44b6304277bf3884ae4f
-      hash = "sha256-oM7q+THBXXw33k+u9OokfusQ5LhankS2MEJ3vziErk8=";
+      # 02b814ccd0e6659468d438fa3be91c0e9934b1f0d5c58078319ea7a6e26729eb
+      hash = "sha256-ArgUzNDmZZRo1Dj6O+kcDpk0sfDVxYB4MZ6npuJnKes=";
     };
 
     nativeBuildInputs = [
