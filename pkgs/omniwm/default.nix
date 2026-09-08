b26d0f5f7a70fa9b9b5bdd0a268145f430fbaecf
alcxyz <alcxyz@noreply.git.alc.xyz>
2026-09-08 04:34:17 +0200

    chore(omniwm): update to 0.6.8 (#329)

--- pkgs/omniwm/default.nix
+++ pkgs/omniwm/default.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "omniwm";
-  version = "0.6.7";
+  version = "0.6.8";
 
   src = fetchurl {
     url = "https://github.com/BarutSRB/OmniWM/releases/download/v${version}/OmniWM-v${version}.zip";
-    hash = "sha256-sg5vshasrk4uv5P2YkSIt1VTlE6rHV4vdv+hirT26Gg=";
+    hash = "sha256-CCOWPIpcO96FT3/dA82MJcRCGkC9b3SETyyPfBaiZ2U=";
   };
 
   nativeBuildInputs = [ unzip ];
