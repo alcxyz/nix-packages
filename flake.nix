fc3bf5af3ec8c633c91a9b8b74d608d9e46ee8fe
forgejo-actions <forgejo-actions@alc.xyz>
2026-09-09 18:17:27 +0200

    chore: retire Jean package

--- flake.nix
+++ flake.nix
@@ -99,7 +99,6 @@
           wcap = pkgs.callPackage ./tools/wcap { };
         }
         // lib.optionalAttrs isLinux {
-          jean = pkgs.callPackage ./pkgs/jean { };
           ndrop = pkgs.callPackage ./pkgs/ndrop { };
           inherit (pkgs) openzfs_7_1;
           stash = pkgs.callPackage ./pkgs/stash { };
